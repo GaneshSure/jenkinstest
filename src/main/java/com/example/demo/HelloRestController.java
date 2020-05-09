d62a8b3970a34901bbe09a702364a7ff6e14b134
GaneshSure <sure.ganeshreddy@gmail.com>
2020-05-09 13:30:00 +0530

    Jenkins Workshop 1st commit

---
+++ src/main/java/com/example/demo/HelloRestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+	
+	@GetMapping(value="/")
+	public String hello() {
+		//return "Welcome to jenkins workshop of Sure Ganesh Reddy/Geetha";
+		return "Hello Nellore People";
+	}
+
+}
